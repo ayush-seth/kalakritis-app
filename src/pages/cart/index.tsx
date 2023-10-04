5b0abb19b6c397e220f35118ffde45fb8766d61f
Ayush Seth <ayush_seth@icloud.com>
2023-10-04 20:48:17 +0530

    fix svg

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -55,23 +55,23 @@ export default function Cart() {
                         <path
                           d="M6.75 16.5C7.16421 16.5 7.5 16.1642 7.5 15.75C7.5 15.3358 7.16421 15 6.75 15C6.33579 15 6 15.3358 6 15.75C6 16.1642 6.33579 16.5 6.75 16.5Z"
                           stroke="#808080"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         />
                         <path
                           d="M15 16.5C15.4142 16.5 15.75 16.1642 15.75 15.75C15.75 15.3358 15.4142 15 15 15C14.5858 15 14.25 15.3358 14.25 15.75C14.25 16.1642 14.5858 16.5 15 16.5Z"
                           stroke="#808080"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         />
                         <path
                           d="M0.75 0.75H3.75L5.76 10.7925C5.82858 11.1378 6.01643 11.448 6.29066 11.6687C6.56489 11.8895 6.90802 12.0067 7.26 12H14.55C14.902 12.0067 15.2451 11.8895 15.5193 11.6687C15.7936 11.448 15.9814 11.1378 16.05 10.7925L17.25 4.5H4.5"
                           stroke="#808080"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         />
                       </g>
                       <defs>
@@ -102,30 +102,30 @@ export default function Cart() {
                       <path
                         d="M13.333 2.5H0.833008V13.3333H13.333V2.5Z"
                         stroke="#808080"
-                        stroke-width="1.66667"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.66667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                       <path
                         d="M13.333 6.66666H16.6663L19.1663 9.16666V13.3333H13.333V6.66666Z"
                         stroke="#808080"
-                        stroke-width="1.66667"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.66667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                       <path
                         d="M4.58333 17.5C5.73393 17.5 6.66667 16.5673 6.66667 15.4167C6.66667 14.2661 5.73393 13.3333 4.58333 13.3333C3.43274 13.3333 2.5 14.2661 2.5 15.4167C2.5 16.5673 3.43274 17.5 4.58333 17.5Z"
                         stroke="#808080"
-                        stroke-width="1.66667"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.66667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                       <path
                         d="M15.4163 17.5C16.5669 17.5 17.4997 16.5673 17.4997 15.4167C17.4997 14.2661 16.5669 13.3333 15.4163 13.3333C14.2657 13.3333 13.333 14.2661 13.333 15.4167C13.333 16.5673 14.2657 17.5 15.4163 17.5Z"
                         stroke="#808080"
-                        stroke-width="1.66667"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.66667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                     </svg>
                   </div>
@@ -151,16 +151,16 @@ export default function Cart() {
                       <path
                         d="M14.8757 2.83334H2.12565C1.34325 2.83334 0.708984 3.46761 0.708984 4.25001V12.75C0.708984 13.5324 1.34325 14.1667 2.12565 14.1667H14.8757C15.6581 14.1667 16.2923 13.5324 16.2923 12.75V4.25001C16.2923 3.46761 15.6581 2.83334 14.8757 2.83334Z"
                         stroke="#808080"
-                        stroke-width="1.41667"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.41667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                       <path
                         d="M0.708984 7.08334H16.2923"
                         stroke="#808080"
-                        stroke-width="1.41667"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.41667"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                     </svg>
                   </div>
