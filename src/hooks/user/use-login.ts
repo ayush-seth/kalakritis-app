881b6faa41a6414cef05d1a488931a1c56719f3e
Ayush Seth <ayush_seth@icloud.com>
2023-10-13 17:02:45 +0530

    fix login

--- src/hooks/user/use-login.ts
+++ src/hooks/user/use-login.ts
@@ -3,7 +3,7 @@ import { client } from "@/utils";
 import NiceModal from "@ebay/nice-modal-react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { setCookie } from "cookies-next";
-import toast from "react-hot-toast";
+import { useRouter } from "next/router";
 
 type LoginInput = { email: string; password: string };
 type LoginResponse = {
@@ -15,18 +15,19 @@ type LoginResponse = {
 
 export function useLogin() {
   const queryClient = useQueryClient();
+  const router = useRouter();
   return useMutation({
     mutationFn: ({ email, password }: LoginInput) =>
       client
         .post("users/token/", { json: { email, password } })
         .json<LoginResponse>(),
-    onSuccess: ({ access, user_id, fullname }) => {
+    onSuccess: ({ access, user_id }) => {
       setCookie("userId", user_id);
       setCookie("token", access);
       NiceModal.remove(LoginModal);
-      toast.success(`Welcome ${fullname}!`);
       queryClient.invalidateQueries({ queryKey: ["wishlist"] });
       queryClient.invalidateQueries({ queryKey: ["cart-details"] });
+      router.reload();
     },
   });
 }
